d72c0f91912ca5aa48845516787e96704f0c926f
RaduD <rdomnu@redhat.com>
2019-07-20 09:34:48 +0200

    Updated setup_jenkins.sh - changed exists condition

--- bin/setup_jenkins.sh
+++ bin/setup_jenkins.sh
@@ -15,7 +15,7 @@ echo "Setting up Jenkins in project ${GUID}-jenkins from Git Repo ${REPO} for Cl
 # Set up Jenkins with sufficient resources
 oc project ${GUID}-jenkins || oc new-project ${GUID}-jenkins
 sleep 2
-app_created=$(oc get bc jenkins-persistent)
+app_created=$(oc get dc jenkins)
 if [[ not $app_created  ]] ; then
     oc new-app jenkins-persistent
     oc set resources dc jenkins --requests=memory=1Gi,cpu=1 --limits=memory=2Gi,cpu=2
